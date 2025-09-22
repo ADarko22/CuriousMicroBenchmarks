6a4e63f176455aa115ff698fbcdbf6fa6a738b1b
ADarko22 <a.elessar22@gmail.com>
2025-09-22 18:40:21 +0200

    Initialize Gradle project with JMH configuration for mico-benchmarking

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java")
+    kotlin("jvm") version "2.2.0"
+    application
+    id("me.champeau.jmh") version "0.7.3"
+}
+
+group = "edu.adarko22"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    jmh("org.openjdk.jmh:jmh-core:1.37")
+    jmhAnnotationProcessor("org.openjdk.jmh:jmh-generator-annprocess:1.37")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+
+rootProject.name = "CuriousMicroBrenchmarks"
